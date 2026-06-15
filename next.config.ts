9baabe5300141bade398aacf915a896f2c2c637f
erikenz <erikenz2001@gmail.com>
2026-06-15 19:17:23 -0300

    configure github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/race-control-studio" : "",
   images: {
     unoptimized: true,
   },
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
